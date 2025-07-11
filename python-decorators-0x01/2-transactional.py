330e65df5501d783e5f2172ac9185fe45798e199
Namisi-Aaron <aaronnamisi@gmail.com>
2025-07-11 16:20:07 +0300

    Transaction Management Decorator 2-transactional.py created

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,42 @@
+import sqlite3
+import os
+from sqlite3 import Error
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, 'my_database.db')
+
+
+def with_db_connection(func):
+    def wrapper(*args, **kwargs):
+        conn = None
+        try:
+            conn = sqlite3.connect(db_path)
+            print(f"Successfully connected to database: {db_path}")
+            result = func(conn, *args, **kwargs)
+            return result
+        except Error as e:
+            print(f"Error connecting to database: {e}")
+        finally:
+            if conn:
+                conn.close()
+                print(f"Connection closed")
+    return wrapper
+
+def transactional(func):
+    def wrapper(connection, *args, **kwargs):
+        try:
+            func(connection, *args, **kwargs)
+            connection.commit()
+        except Error as e:
+            print(f"An error occured: {e}")
+            connection.rollback()
+        print("Transaction committed successfully")
+    return wrapper
+
+@with_db_connection 
+@transactional
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+
+update_user_email(user_id=1, new_email='crawford_cartwright@hotmail.com')
